c5c6f37e86928354dcea8980ba0a2f9224b926a8
CarinaoHC <karinaina678@gmail.com>
2024-01-25 09:17:10 -0400

    dogs

---
+++ semana04/dogs/src/app/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Dog from "./components/dogs";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Dog />
+    </main>
+  );
+}
