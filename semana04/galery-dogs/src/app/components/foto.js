bc2f0aa44436ee016fd0690fb9d6c95e4cd5f32c
CarinaoHC <karinaina678@gmail.com>
2024-01-26 01:17:02 -0400

    galery-pokemon

---
+++ semana04/galery-dogs/src/app/components/foto.js
@@ -0,0 +1,27 @@
+"use client"
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import style from "./foto.module.css";
+
+function Foto() {
+    const [perrito, setPerrito] = useState("/vercel.svg");
+    const [estado, setEstado] = useState("esperando");
+    const [habilidad, setHabilidad] = useState("habilidad");
+    const url = "https://pokeapi.co/api/v2/pokemon/lucario";
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setPerrito(data.sprites.front_default),
+                setEstado(data.species.name),setHabilidad(data.abilities[0].ability.name)
+            });
+    }, [])
+    return (
+        <div className={style.contenedor}>
+            <h1>{estado}</h1>
+            <Image src={perrito} alt="dog" width={500} height={500} />
+            <h1>{habilidad}</h1>
+        </div>
+    );
+}
+export default Foto;
